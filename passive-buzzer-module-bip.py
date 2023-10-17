96f0f2ce925ad1109101466ed8a3f90f8ad7f25b
Lucas Rocha <dev.lucascrocha@gmail.com>
2023-10-17 00:49:09 +0000

    Makes the bip louder

--- passive-buzzer-module-bip.py
+++ passive-buzzer-module-bip.py
@@ -9,13 +9,10 @@ from time import sleep
 # Creates buzzer instance at Pin 15 due to being connected into the GPIO Pin 15
 # See passive-buzzer-module-bip-schema.png to visualize the Pin 15 connection and ground connection
 buzzer = PWM(Pin(15))
-buzzer.freq(5000)
+buzzer.freq(2000)
 
 while True:
-    buzzer.duty_u16(32768) 
+    buzzer.duty_u16(49152) 
     sleep(0.5)
     buzzer.duty_u16(0)
     sleep(0.5)
-
-
-
